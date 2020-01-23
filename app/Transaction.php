64d71fad8140df36b74071907447fc07bef81991
Banjo Mofesola Paul <mpdepaule1@gmail.com>
2020-01-23 23:54:58 +0200

    Specified model relationships

--- app/Transaction.php
+++ app/Transaction.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    public function sender()
+    {
+        return $this->hasOne(Account::class, null, 'sender_id');
+    }
+
+    public function receiver()
+    {
+        return $this->hasOne(Account::class, null, 'receiver_id');
+    }
 }
